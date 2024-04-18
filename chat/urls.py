cef71030386fb79c921065b1c0581a244cd641c0
clintonjohn-cmd <johnouko@kabarak.ac.ke>
2024-04-18 22:25:45 +0300

    Latest views

--- chat/urls.py
+++ chat/urls.py
@@ -4,3 +4,49 @@ from .import views
 urlpatterns = [
     path('', views.home, name='chatHome'),
 ]
+
+urlpatterns = [
+    ###Basic Website functionalities
+    path('', views.home , name="home"),
+    path('home/', views.home , name="home"),
+    path('signup/', views.signup , name="signup"),
+    path('login/', views.loginPage , name="login"),
+    path('logout/', views.logoutPage , name="logout"),
+
+    #### The user profile and its functionalities
+    path('userProfile/<str:pk>/', views.userProfile , name="user_profile"),
+    path('updateProfile/', views.updateProfile , name="update_profile"),
+    path('deleteUser/<str:pk>/', views.deleteUser , name="delete_user"),
+
+    ### The Events section and related functionalities
+    path('addEvent/', views.addEvent , name="add_event"),
+    path('event/<str:pk>/', views.viewEvent , name="view_event"),
+    path('updateEvent/<str:pk>/', views.updateEvent , name="update_event"),
+    path('deleteEvent/<str:pk>/', views.deleteEvent , name="delete_event"),
+    
+    ##The sports Event section
+    path('viewSportsEvents/', views.sportsEvent , name="view_sports_events"),
+    path('addSportsEvent/', views.addSportsEvent , name="add_sports_event"),
+    path('sportEvent/<str:pk>/', views.viewSportEvent , name="view_sports_event"),
+    path('updateSportsEvent/<str:pk>/', views.updateSportsEvent , name="update_sports_event"),
+    path('deleteSportsEvent/<str:pk>/', views.deleteSportsEvent , name="delete_sports_event"),
+
+    ##The Teams section
+    path('addTeam/', views.addTeam , name="add_team"),
+    path('leagueTable/', views.viewTable , name="league_table"),
+    path('manageLeagueTable/', views.manageLeagueTable , name="manage_league_table"),
+    path('updateTeam/<str:pk>/', views.updateTeam , name="update_team"),
+
+    
+    ### The Administrators Page
+    path('adminsPage/', views.adminsPage ,  name="admins_page"),
+    path('changeRole/', views.changeRole ,  name="change_role"),
+    path('addSportsAdmin/', views.addSportsAdmin ,  name="add_sports_admin"),
+
+      #email configuration and password reset section
+    path('resetPassword/', auth_views.PasswordResetView.as_view(template_name='base/password_reset.html'), name="reset_password" ),
+    path('resetPasswordSent/', auth_views.PasswordResetDoneView.as_view(template_name='base/password_reset_sent.html'),name="password_reset_done" ),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='base/password_reset_form.html'), name="password_reset_confirm"),
+    path('resetPasswordComplete/', auth_views.PasswordResetCompleteView.as_view(template_name='base/password_reset_done.html'),name="password_reset_complete"),
+
+]
--- chat/views.py
+++ chat/views.py
@@ -75,3 +75,123 @@ def addSportsAdmin(request):
 
    context = {}
    return render(request , 'base/change_role.html' , context)
+
+
+#---------------------------Basic Website Functions ------------------------------
+
+def home(request):
+   events = Event.objects.all()
+   second_events = Event.objects.all()[0:8]
+   topics = Topic.objects.all()[0:4]
+
+   sports_events = Sport_Event.objects.all()[0:4]
+
+   group = Group.objects.get(name='super_admin')
+   admin_group = Group.objects.get(name='Admins')
+   sports_admins = Group.objects.get(name='sports_admins')
+   
+   sports_admins_users = sports_admins.user_set.all()
+   admin_group_users = admin_group.user_set.all()
+   group_super_admin = group.user_set.all()
+
+
+   context = {'events':events, 'group_super_admin':group_super_admin, 'admin_group_users':admin_group_users, 'sports_admins_users':sports_admins_users, 'sports_events':sports_events, 'topics':topics}
+   return render(request , 'base/home.html', context)
+
+def signup(request):
+    page = 'signup'
+
+    if request.user.is_authenticated:
+      return redirect('home')
+
+    form = MyUserCreationForm()
+    
+    if request.method == 'POST':
+      form = MyUserCreationForm(request.POST)
+      if form.is_valid():
+         user = form.save(commit=False)
+         user.username = user.username.lower()
+         user.save()
+
+         group = Group.objects.get(name='Students')
+         user.groups.add(group)
+         username = form.cleaned_data.get('username')
+         messages.success(request, f'Account successfully created for {username}')
+
+         login(request, user)
+         return redirect('home')
+      # else:
+      #    messages.error(request , 'An error has occured during registration')
+
+
+    context = {'page':page, 'form':form}
+    return render(request , 'base/login_register.html', context)
+
+@unauthenticated_user
+def loginPage(request):
+   page = 'login'
+
+   if request.method == 'POST':
+      username = request.POST.get('username')
+      password = request.POST.get('password')
+
+      try:
+         user = User.objects.get(username=username)
+         
+      except:
+         messages.error(request, 'User does not exist')
+      
+      user = authenticate(request, username=username , password=password)
+      if user is not None:
+         login(request, user)
+         return redirect('home')
+      else:
+         messages.error(request, 'Username or Password doesnt exist')
+
+
+   context = {'page':page}
+   return render(request, 'base/login_register.html', context)
+
+
+def logoutPage(request):
+    logout(request)
+    return redirect('home')
+
+def sportsEvent(request):
+   sports_events = Sport_Event.objects.all()
+   context = {'sports_events':sports_events}
+   return render(request, 'base/events_component.html', context)
+
+
+
+#---------------------------User Profile functions ------------------------------
+def userProfile(request, pk):
+   user = User.objects.get(id=pk)
+   events = user.event_set.all()
+
+
+   context = {'user':user, 'events':events}
+   return render(request, 'base/profile.html', context)
+
+@login_required(login_url='login')
+def updateProfile(request):
+   user = request.user
+   update_form = UserProfileForm(instance=user)
+   if request.method == 'POST':
+      update_form = UserProfileForm(request.POST,request.FILES, instance=user)
+      if update_form.is_valid():
+         update_form.save()
+         return redirect('user_profile' , pk=user.id)
+ 
+   context = {'update_form':update_form}
+   return render(request,'base/update_profile.html', context)
+
+@login_required(login_url='login')
+def deleteUser(request, pk):
+   user = User.objects.get(id=pk)
+
+   if request.method == 'POST':
+      user.delete()
+      return  redirect('home')
+
+   return render(request, 'base/delete.html' , {'obj' :user})
